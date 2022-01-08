103f98edd4bd581e22e18c6345752aa6795e24f8
Raffi V <rafivar@gmail.com>
2022-01-08 15:07:58 +0200

    Added descrtiption for ThreeInOne

--- src/main/kotlin/chapter_3_stacks_and_queues/S1ThreeInOne.kt
+++ src/main/kotlin/chapter_3_stacks_and_queues/S1ThreeInOne.kt
@@ -1,4 +1,10 @@
 package chapter_3_stacks_and_queues
 
 class S1ThreeInOne {
+    /*
+        In order to solve this, we can assume that the array is divided to 3 equal sections
+        Each section will have its own pointer (p1, p2, p3)
+        Each pointer will represent the top of the stack
+        Representation of empty stack: p is in the beginning of section + value is null
+     */
 }

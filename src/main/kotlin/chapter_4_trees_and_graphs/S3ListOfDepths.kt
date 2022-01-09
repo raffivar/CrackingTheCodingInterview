941bef7a80c41e8776f706ae4f26627021fbbaf0
Raffi V <rafivar@gmail.com>
2022-01-09 15:18:14 +0200

    Added solution for List Of Depths

---
+++ src/main/kotlin/chapter_4_trees_and_graphs/S3ListOfDepths.kt
@@ -0,0 +1,37 @@
+package chapter_4_trees_and_graphs
+
+import chapter_4_trees_and_graphs.helpers.trees.binary.Node
+import chapter_4_trees_and_graphs.helpers.trees.TreeUtil
+
+class S3ListOfDepths {
+    private fun listOfDepths(root: Node?): List<List<Node?>> {
+        val lists = mutableListOf<MutableList<Node?>>()
+        if (root == null) {
+            return lists
+        }
+        addDepths(root, lists, 0)
+        return lists
+    }
+
+    private fun addDepths(root: Node, lists: MutableList<MutableList<Node?>>, level: Int) {
+        if (level >= lists.size) { //new depth
+            val list = mutableListOf<Node?>()
+            lists.add(list)
+        }
+        val depthList = lists[level]
+        depthList.add(root)
+        root.left?.let {
+            addDepths(it, lists, level + 1)
+        }
+        root.right?.let {
+            addDepths(it, lists, level + 1)
+        }
+    }
+
+    fun runTest() {
+        val root = TreeUtil.buildBinaryTree3()
+        //TreeUtil.printBinaryTree(root)
+        val depthsList = listOfDepths(root)
+        //TreeUtil.printDepthsList(depthsList)
+    }
+}
--- src/main/kotlin/chapter_4_trees_and_graphs/helpers/trees/TreeUtil.kt
+++ src/main/kotlin/chapter_4_trees_and_graphs/helpers/trees/TreeUtil.kt
@@ -5,7 +5,6 @@ import chapter_4_trees_and_graphs.helpers.trees.binary.Node
 object TreeUtil {
     fun printBinaryTree(root: Node?) {
         printBinaryTree(root, 0)
-
     }
 
     private fun printBinaryTree(root: Node?, level: Int) {
@@ -23,5 +22,82 @@ object TreeUtil {
         }
 
         printBinaryTree(root.left, level + 1)
+    }
+
+//    fun printDepthsList(list: List<List<Node?>>) {
+//        var level = 0
+//        for (depth in list) {
+//            for (node in depth) {
+//                if (node == null) {
+//                    print("?".padStart(list.size - level + 1))
+//                } else {
+//                    print("${node.value}".padStart(list.size - level + 1))
+//                }
+//            }
+//            println()
+//            if (level < list.size - 1) {
+//                for (node in depth) {
+//                    print("/ \\".padStart(list.size - level + 2))
+//                }
+//            }
+//            println()
+//            level++
+//        }
+//    }
+
+    fun buildBinaryTree(): Node {
+        return Node(
+            1,
+            Node(
+                2,
+                Node(4),
+                Node(5)
+            ),
+            Node(
+                3,
+                Node(6),
+                Node(7)
+            )
+        )
+    }
+
+    fun buildBinaryTree2(): Node {
+        return Node(
+            1,
+            Node(
+                2,
+                Node(4),
+                Node(5)
+            ),
+            Node(
+                3,
+                Node(6),
+            )
+        )
+    }
+
+    fun buildBinaryTree3(): Node {
+        return Node(
+            1,
+            Node(
+                2,
+                Node(4),
+                Node(5)
+            ),
+            Node(
+                3,
+                null,
+                Node(7)
+            )
+        )
+    }
+
+
+    fun buildBinaryTree4(): Node {
+        return Node(1)
+    }
+
+    fun buildBinaryTree5(): Node? {
+        return null
     }
 }

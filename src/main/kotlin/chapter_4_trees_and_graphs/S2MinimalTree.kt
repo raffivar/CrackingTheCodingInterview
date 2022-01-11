d1cb7da50c416d0d03356fa10f504e6d9b05a503
Raffi V <rafivar@gmail.com>
2022-01-11 20:03:49 +0200

    Improved code

--- src/main/kotlin/chapter_4_trees_and_graphs/S2MinimalTree.kt
+++ src/main/kotlin/chapter_4_trees_and_graphs/S2MinimalTree.kt
@@ -8,18 +8,24 @@ class S2MinimalTree {
         if (array.isEmpty()) {
             return null
         }
-        
         val middleIndex = array.size / 2
         val root = TreeNode(array[middleIndex])
-
         root.left = buildMinimalTree(array.sliceArray(0 until middleIndex))
         root.right = buildMinimalTree(array.sliceArray(middleIndex + 1 until array.size))
         return root
     }
 
     fun runTest() {
-        val array = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
-        val root = buildMinimalTree(array)
-        TreeUtil.printBinaryTreeViaDepths(root)
+        val functions = arrayListOf(this::buildMinimalTree)
+        val testCases = arrayListOf(
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
+            arrayOf(10, 20, 30, 40, 50, 60, 70)
+        )
+        for (function in functions) {
+            for (testCase in testCases) {
+                val result = function(testCase)
+                TreeUtil.printBinaryTreeViaDepths(result)
+            }
+        }
     }
 }

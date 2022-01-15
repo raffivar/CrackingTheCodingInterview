b6185db0767d255e914383d9a835018247f98c81
Raffi V <rafivar@gmail.com>
2022-01-15 16:41:53 +0200

    Added solution for coins

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
+import chapter_5_recursion.S11Coins
 import chapter_5_recursion.S1TripleStep
 
 fun main() {
-    val solution = S1TripleStep()
+    val solution = S11Coins()
     solution.runTest()
 }
---
+++ src/main/kotlin/chapter_5_recursion/S11Coins.kt
@@ -0,0 +1,40 @@
+package chapter_5_recursion
+
+class S11Coins {
+    private fun possibleCoins(numOfCents: Int, possibleCoins: ArrayList<Int>): Int {
+        return coins(0, numOfCents, possibleCoins)
+    }
+
+    private fun coins(current: Int, total: Int, possibleCoins: ArrayList<Int>): Int {
+        if (current == total) {
+            println("======================= REACHED SUM =========================")
+            return 1
+        }
+        var options = 0
+        for ((i, coin) in possibleCoins.withIndex()) {
+            val subCoins = ArrayList(possibleCoins.slice(i..possibleCoins.lastIndex))
+            val sum = current + coin
+            if (sum <= total) {
+                println("[$current/$total] -> [$sum/$total] -> added [$coin]")
+                options += coins(sum, total, subCoins)
+            }
+        }
+        return options
+    }
+
+
+    fun runTest() {
+        val functions = arrayListOf(this::possibleCoins)
+        val testCases = arrayListOf(
+            Pair(15, arrayListOf(10, 5, 1))
+        )
+        for (function in functions) {
+            for (case in testCases) {
+                println("------------------------------------------")
+                println("stairs: ${case.first}")
+                println("step sizes: ${case.second}")
+                println("number of ways to represent [${case.first}] cents:  ${function(case.first, case.second)}")
+            }
+        }
+    }
+}

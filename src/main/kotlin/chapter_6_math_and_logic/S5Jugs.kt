725e77ddac592f2bbc38a12497c25481bbb11331
Raffi V <rafivar@gmail.com>
2022-01-20 14:00:18 +0200

    Added solution for Jugs

---
+++ src/main/kotlin/chapter_6_math_and_logic/S5Jugs.kt
@@ -0,0 +1,13 @@
+package chapter_6_math_and_logic
+
+class S5Jugs {
+    /**
+     * Fill 5 ->
+     * Pour from 5 to 3 ->
+     * Spill 3 ->
+     * Pour from 5 to 3 ->
+     * Fill 5 ->
+     * Pour from 5 to 3 ->
+     * 4 remain in the bigger jug
+     */
+}

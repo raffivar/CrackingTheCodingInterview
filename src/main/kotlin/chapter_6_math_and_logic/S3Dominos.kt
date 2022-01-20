45b2097ee5a1edffd96a9a0e92b7321235cda6a9
Raffi V <rafivar@gmail.com>
2022-01-20 13:37:39 +0200

    Added solution for Dominos

---
+++ src/main/kotlin/chapter_6_math_and_logic/S3Dominos.kt
@@ -0,0 +1,12 @@
+package chapter_6_math_and_logic
+
+class S3Dominos {
+    /**
+     * The board initially has 64 tiles.
+     * We take out two opposite diagonal tiles, and remain with 62 tiles.
+     * We have 31 domino pieces, each covers two tiles.
+     * Each one of the domino pieces should cover 1 black tile and 1 white tile.
+     * However, we removed two pieces of the same color.
+     * Hence, it's impossible.
+     */
+}

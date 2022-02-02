407e5e3b234ec51edcee784efed1ef4bd79f09ff
Raffi V <rafivar@gmail.com>
2022-02-02 14:26:04 +0200

    Added solution for thread vs process

---
+++ src/main/kotlin/chapter_15_threads_and_locks/S1ThreadVSProcess.kt
@@ -0,0 +1,21 @@
+package chapter_15_threads_and_locks
+
+class S1ThreadVSProcess {
+    /**
+     * Question:
+     * What is the difference between a thread and a process?
+     *
+     * Answer:
+     * Processes are separate units of program.
+     * Each process  is executed in a separate address space.
+     * They cannot access each other's resources unless done explicitly,
+     * by using pipes, files, sockets, etc.
+     *
+     * A process can contain several threads.
+     * A thread exists within a process and shares the process' resource (including heap space).
+     * Multiple threads within the same process will share the same heap space.
+     * Each thread still has its own registers and its own stack,
+     * but other thread can read and write the heap memory.
+     *
+     */
+}

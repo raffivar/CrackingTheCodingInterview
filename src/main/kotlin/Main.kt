1d7c81ad6bf1184bca4511cf7179be9d56b6fe69
Raffi V <rafivar@gmail.com>
2022-02-03 16:15:00 +0200

    Added solution for dining philosophers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 import chapter_15_threads_and_locks.S3DiningPhilosophers
-import chapter_15_threads_and_locks.examples.ATM
 
 fun main() {
     val solution = S3DiningPhilosophers()
--- src/main/kotlin/chapter_15_threads_and_locks/S3DiningPhilosophers.kt
+++ src/main/kotlin/chapter_15_threads_and_locks/S3DiningPhilosophers.kt
@@ -1,11 +1,67 @@
 package chapter_15_threads_and_locks
 
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.random.Random
+
 class S3DiningPhilosophers {
-    class Chopstick(val name: String)
-    class Philosopher(
-        var left: Chopstick?,
-        val right: Chopstick?
-    )
+    class Chopstick(val name: String) : ReentrantLock()
+    class Philosopher(private val pName: String, var left: Chopstick?, val right: Chopstick?) : Thread() {
+        var dineTime = 5000L
+        var cooldownTime = 500L
+        var minWaitTime = 500
+        var maxWaitTime = 1500
+
+        override fun run() {
+            while (true) {
+                //pick up left
+                val timeToPickUpChopsticks = Random.nextInt(minWaitTime, maxWaitTime).toLong()
+                sleep(timeToPickUpChopsticks)
+                when (!left!!.isLocked) {
+                    true -> {
+                        left!!.lock()
+                        println("$pName picked up left [${left!!.name}]")
+                    }
+                    false -> {
+                        println("$pName could not pick up left [${left!!.name}], trying again in [$cooldownTime] ms")
+                        sleep(cooldownTime)
+                        continue
+                    }
+                }
+                //pick up right
+                when (!right!!.isLocked) {
+                    true -> {
+                        right!!.lock()
+                        println("$pName picked up right [${right!!.name}]")
+                    }
+                    false -> {
+                        println("$pName could not pick up right [${right!!.name}], freeing left [${left!!.name}] + trying again in [$cooldownTime] ms")
+                        freeLeft()
+                        sleep(cooldownTime)
+                        continue
+                    }
+                }
+                println("$pName started dining")
+                sleep(dineTime)
+                freeLeft()
+                freeRight()
+                println("$pName stopped dining")
+            }
+        }
+
+        private fun freeLeft() {
+            if (left!!.isHeldByCurrentThread) {
+                left!!.unlock()
+                println("$pName put down left [${left!!.name}]")
+            }
+        }
+
+        private fun freeRight() {
+            if (right!!.isHeldByCurrentThread) {
+                right!!.unlock()
+                println("$pName put down right [${right.name}]")
+            }
+        }
+    }
 
     class DiningTable(n: Int) {
         private val philosophers = arrayListOf<Philosopher>()
@@ -13,26 +69,24 @@ class S3DiningPhilosophers {
         init {
             for (i in 0 until n) {
                 val philosopher = when (i) {
-                    0 -> Philosopher(null, Chopstick("Chopstick #1"))
-                    n - 1 -> Philosopher(philosophers[i - 1].right, Chopstick("Chopstick #0"))
-                    else -> Philosopher(philosophers[i - 1].right, Chopstick("Chopstick #${i + 1}"))
+                    0 -> Philosopher("Philosopher #$i", null, Chopstick("Chopstick #1"))
+                    n - 1 -> Philosopher("Philosopher #$i", philosophers[i - 1].right, Chopstick("Chopstick #0"))
+                    else -> Philosopher("Philosopher #$i", philosophers[i - 1].right, Chopstick("Chopstick #${i + 1}"))
                 }
                 philosophers.add(philosopher)
             }
             philosophers[0].left = philosophers.last().right
         }
 
-        fun printState() {
-            for ((i, p) in philosophers.withIndex()) {
-                println("Philosopher #$i left: ${p.left?.name}")
-                println("Philosopher #$i right: ${p.right?.name}")
-                println("------------------------------------------------")
+        fun startDining() {
+            for (philosopher in philosophers) {
+                philosopher.start()
             }
         }
     }
 
     fun runTest() {
         val table = DiningTable(4)
-        table.printState()
+        table.startDining()
     }
 }

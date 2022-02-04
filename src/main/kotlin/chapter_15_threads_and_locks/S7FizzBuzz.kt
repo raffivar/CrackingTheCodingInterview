6fdb632d2defd7dc2100e8edbcf6ebebe1b1d142
Raffi V <rafivar@gmail.com>
2022-02-04 22:16:45 +0200

    Improved code (now instead of running through the numbers 4 times, once per thread, sharing the number as a resource between all threads)

--- src/main/kotlin/chapter_15_threads_and_locks/S7FizzBuzz.kt
+++ src/main/kotlin/chapter_15_threads_and_locks/S7FizzBuzz.kt
@@ -1,15 +1,33 @@
 package chapter_15_threads_and_locks
 
 class S7FizzBuzz {
-    private class FizzBuzzThread(private val n: Int, private val div3: Boolean, private val div5: Boolean) : Thread() {
+    private class FizzBuzzThread(
+        private val n: Int,
+        private val div3: Boolean,
+        private val div5: Boolean,
+        private val toPrint: String?
+    ) : Thread() {
+        companion object {
+            @JvmStatic
+            var lock = Object()
+
+            @JvmStatic
+            var i = 1
+        }
+
         override fun run() {
-            for (i in 1..n) {
-                sleep(100)
-                when {
-                    div3 && div5 && i % 15 == 0 -> println("$i [FizzBuzz]")
-                    div3 && !div5 && i % 3 == 0 -> println("$i [Fizz]")
-                    !div3 && div5 && i % 5 == 0 -> println("$i [Buzz]")
-                    !div3 && !div5 && i % 3 != 0 && i % 5 != 0 -> println(i)
+            while (true) {
+                if (i >= n) {
+                    return
+                }
+                synchronized(lock) {
+                    if ((i % 3 == 0) == div3 && (i % 5 == 0) == div5) {
+                        when (toPrint) {
+                            null -> println(i)
+                            else -> println(toPrint)
+                        }
+                        i++
+                    }
                 }
             }
         }
@@ -17,9 +35,9 @@ class S7FizzBuzz {
 
     fun runTest() {
         val n = 100
-        FizzBuzzThread(n, div3 = true, div5 = false).start()
-        FizzBuzzThread(n, div3 = false, div5 = true).start()
-        FizzBuzzThread(n, div3 = true, div5 = true).start()
-        FizzBuzzThread(n, div3 = false, div5 = false).start()
+        FizzBuzzThread(n, div3 = true, div5 = true, "FizzBuzz").start()
+        FizzBuzzThread(n, div3 = true, div5 = false, "Fizz").start()
+        FizzBuzzThread(n, div3 = false, div5 = true, "Buzz").start()
+        FizzBuzzThread(n, div3 = false, div5 = false, null).start()
     }
 }

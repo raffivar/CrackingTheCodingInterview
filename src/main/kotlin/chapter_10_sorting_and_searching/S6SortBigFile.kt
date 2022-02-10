de7644c0e519b502421bd03268e3f12e967a84ce
Raffi V <rafivar@gmail.com>
2022-02-10 14:32:18 +0200

    Added solution for sorted big file

---
+++ src/main/kotlin/chapter_10_sorting_and_searching/S6SortBigFile.kt
@@ -0,0 +1,13 @@
+package chapter_10_sorting_and_searching
+
+class S6SortBigFile {
+    /**
+     * Let x be the amount of memory we are given to work with
+     * 1. Divide the file into chunks, which are x megabytes each
+     * Each chunk is sorted separately and then saved back to the file system
+     * 2. Once all the chunks are sorted, we merge the chunks, one by one.
+     * At the end, we have a fully sorted file.
+     *
+     * This algorithm is known as external sort.
+     */
+}

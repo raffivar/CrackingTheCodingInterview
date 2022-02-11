97ae8187db4144095c981fe7e51cea094db363b2
Raffi V <rafivar@gmail.com>
2022-02-11 16:53:19 +0200

    Added infrastructure for chaprer 13 - Java

---
+++ src/main/kotlin/chapter_13_java/S1toS8.kt
@@ -0,0 +1,49 @@
+package chapter_13_java
+
+class S1toS8 {
+    /**
+     * S1 - Private Constructor:
+     * Q: In terms of inheritance, what is the effect of keeping a constructor private?
+     * A:
+     *
+     *
+     * S2 - Return from Finally:
+     * Q: In Java, does the finally block get executed if we insert a return statement inside the try block of a try-catch-finally?
+     * A:
+     *
+     *
+     * S3 - Final,etc.
+     * Q: What is the difference between final, finally, and finalize?
+     * A:
+     *
+     *
+     * S4 - Generics vs. Templates
+     * Q: Explain the difference between templates in C++ and generics in Java.
+     * A:
+     *
+     *
+     * S5 - TreeMap, HashMap, LinkedHashMap:
+     * Q: Explain the differences between TreeMap, HashMap, and inkedHashMap. Provide an example of when each one would be best.
+     * A:
+     *
+     *
+     * S6 - Object Reflection:
+     * Q: Explain what object reflection is in Java and why it is useful.
+     * A:
+     *
+     *
+     * S7 - Lambda Expressions:
+     * Q: There is a class Country that has methods getContinent() and
+     * getPopulation(). Write a function int getPopulation(List<Country> countries,
+     * String continent) that computes the total population of a given continent, given a list of all
+     * countries and the name of a continent.
+     *
+     *
+     * S8 - Lambda Random:
+     * Q: Using lambda expressions, write a function List<Integer>getRandomSubset(List<Integer> list)
+     * that returns a random subset of arbitrary size.
+     * All subsets (including the empty set) should be equally likely to be chosen
+     *
+     *
+     */
+}

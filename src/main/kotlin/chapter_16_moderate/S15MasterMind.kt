11916cf3db20c5f97a542a99c6fd2963d5829d23
Raffi V <rafivar@gmail.com>
2022-02-14 17:05:05 +0200

    Added solution for MasterMind

--- src/main/kotlin/chapter_16_moderate/S15MasterMind.kt
+++ src/main/kotlin/chapter_16_moderate/S15MasterMind.kt
@@ -4,23 +4,33 @@ class S15MasterMind {
     private val n = 4
     private val colors = arrayOf('R', 'Y', 'G', 'B')
 
+    private enum class HitValue { Miss, PseudoHit, Hit }
+
     private fun startGame() {
         val sb = StringBuilder()
         for (i in 1..n) {
             sb.append(colors.random())
         }
-        val secret = sb.toString()
+        //val secret = sb.toString()
+        val secret = "GBGG"
         println("secret: $secret")
         do {
+            println("Enter your guess:")
             val guess = readln()
             when (isInputValid(guess)) {
-                true -> println("input is valid")
+                true -> {
+                    val hits = calculateHits(secret, guess)
+                    for (hit in hits) {
+                        print("[$hit]")
+                    }
+                    println()
+                }
                 false -> println("input is invalid")
             }
         } while (!guess.equals(secret, true))
         print("You guessed the word!")
     }
-    
+
     private fun isInputValid(input: String): Boolean {
         if (input.length != n) {
             return false
@@ -33,6 +43,35 @@ class S15MasterMind {
         }
 
         return true
+    }
+
+    private fun calculateHits(secret: String, guess: String): Array<HitValue> {
+        //Init all to be miss
+        val result = Array(secret.length) { HitValue.Miss }
+        val secretMap = HashMap<Int, Char>()
+
+        //Take care of hits
+        for ((i, char) in secret.withIndex()) {
+            when (secret[i] == guess[i]) {
+                true -> result[i] = HitValue.Hit
+                false -> secretMap[i] = char
+            }
+        }
+
+        //Take care of pseudo hits
+        for (i in 0..guess.lastIndex) {
+            if (result[i] != HitValue.Hit) {
+                for (key in secretMap.keys) {
+                    if (guess[i] == secretMap[key]) {
+                        result[i] = HitValue.PseudoHit
+                        secretMap.remove(key)
+                        break
+                    }
+                }
+            }
+        }
+
+        return result
     }
 
     fun runTest() {

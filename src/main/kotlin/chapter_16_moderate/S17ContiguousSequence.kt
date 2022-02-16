63b5d01b3cfe9838495b754b74621a5acc661ade
Raffi V <rafivar@gmail.com>
2022-02-17 00:39:51 +0200

    Beautified code

--- src/main/kotlin/chapter_16_moderate/S17ContiguousSequence.kt
+++ src/main/kotlin/chapter_16_moderate/S17ContiguousSequence.kt
@@ -3,29 +3,27 @@ package chapter_16_moderate
 class S17ContiguousSequence {
     private fun getMaxSum(array: IntArray): Int {
         val n = array.size
-
         val sumArray = ArrayList<Int>()
-        var left = 0
         var right = 0
-
         for (i in 1..n) {
-            if (i == 1) { //first cycle
-                for (num in array) {
-                    sumArray.add(num)
-                }
-                left = 0
-                right = sumArray.size
-            } else {
-                val delta = right - left
-                for (j in 0..delta) {
-                    if (j + i - 1 < array.size) {
-                        sumArray.add(sumArray[left + j] + array[j + i - 1])
+            when (i) {
+                1 -> { //first cycle
+                    for (num in array) {
+                        sumArray.add(num)
                     }
                 }
-                left = right
-                right = sumArray.size
+                else -> {
+                    val left = right
+                    right = sumArray.size
+                    for (j in 0..right - left) {
+                        if (j + i - 1 < array.size) {
+                            sumArray.add(sumArray[left + j] + array[j + i - 1])
+                        }
+                    }
+                }
             }
         }
+
         var maxSum = Int.MIN_VALUE
         for (sum in sumArray) {
             if (sum > maxSum) {

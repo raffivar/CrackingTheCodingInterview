6673d5bd10087848c5a699556f177ece7e6cf7b2
Raffi V <rafivar@gmail.com>
2022-02-16 18:13:50 +0200

    Fixed bug + Improved code

--- src/main/kotlin/chapter_16_moderate/S23Rand7FromRand5.kt
+++ src/main/kotlin/chapter_16_moderate/S23Rand7FromRand5.kt
@@ -6,13 +6,26 @@ class S23Rand7FromRand5 {
     private fun rand7(): Int {
         var result = 0
         do {
-            when (val newValue = result + rand5()){
-                in 0..6 -> result = newValue
-                else -> break
+            val newValue = result + rand5()
+            if (newValue in 0..6) {
+                result = newValue
             }
-        } while (rand5() + rand5() in 0..4) //flip coin [0..4 = continue, 5..9 = break]
-
+        } while (rollAgain())
         return result
+    }
+
+    /**
+     * Flips coin
+     * 0, 1 -> yes
+     * 3, 4 -> no
+     * 2 -> roll again
+     */
+    private fun rollAgain(): Boolean {
+        return when (rand5()) {
+            in 0..1 -> true
+            in 3..4 -> false
+            else -> rollAgain()
+        }
     }
 
     private fun rand5(): Int {

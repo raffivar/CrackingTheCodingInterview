eb523f8723ee94d3d34a2897766dddcbc912e29e
Raffi V <rafivar@gmail.com>
2022-02-17 18:21:18 +0200

    Added solution for LRU Cache

--- src/main/kotlin/chapter_16_moderate/S25LRUCache.kt
+++ src/main/kotlin/chapter_16_moderate/S25LRUCache.kt
@@ -1,15 +1,38 @@
 package chapter_16_moderate
 
 class S25LRUCache {
-    class LRU {
-        private val map = HashMap<Int, String>()
+    class LRU(private val maxSize: Int) {
+        private val map = HashMap<Int, StringWithTimeStamp>()
+
         fun add(key: Int, value: String) {
-            map[key] = value
+            if (map.size == maxSize) {
+                findOldest()?.let {
+                    map.remove(findOldest())
+                }
+            }
+            map[key] = StringWithTimeStamp(value)
         }
+
+        private fun findOldest(): Int? {
+            var minTimeStamp = Long.MAX_VALUE
+            var oldestKey: Int? = null
+            for (pair in map) {
+                if (pair.value.timeStamp < minTimeStamp) {
+                    minTimeStamp = pair.value.timeStamp
+                    oldestKey = pair.key
+                }
+            }
+            return oldestKey
+        }
+
         fun printState() {
             for (pair in map) {
-                println("${pair.key} -> ${pair.value}")
+                println("${pair.key} -> ${pair.value.value}")
             }
+        }
+
+        private class StringWithTimeStamp(val value: String) {
+            val timeStamp = System.currentTimeMillis()
         }
     }
 
@@ -23,7 +46,7 @@ class S25LRUCache {
             Pair(6, "six"),
             Pair(7, "seven")
         )
-        val cache = LRU()
+        val cache = LRU(4)
         for (pair in pairs) {
             cache.add(pair.first, pair.second)
             println("current state:")

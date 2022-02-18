d368edf8f699cbab28c144b6d04c92ec0ed66ca5
Raffi V <rafivar@gmail.com>
2022-02-18 23:29:09 +0200

    Added solution for living people

--- src/main/kotlin/chapter_16_moderate/S10LivingPeople.kt
+++ src/main/kotlin/chapter_16_moderate/S10LivingPeople.kt
@@ -1,8 +1,33 @@
 package chapter_16_moderate
 
+import chapter_10_sorting_and_searching.sorting.Sorting
+
 class S10LivingPeople {
     private fun getMaxYear(people: ArrayList<Person>): Int {
-        return 0
+        val yearPopulations = HashMap<Int, Int>()
+        for (person in people) {
+            when (val birth = yearPopulations[person.birth]) {
+                null -> yearPopulations[person.birth] = 1
+                else -> yearPopulations[person.birth] = birth + 1
+            }
+            when (val death = yearPopulations[person.death]) {
+                null -> yearPopulations[person.death + 1] = -1
+                else -> yearPopulations[person.death + 1] = death - 1
+            }
+        }
+        val years = yearPopulations.keys.toIntArray()
+        Sorting.sort(years)
+        var sum = 0
+        var maxSum = 0
+        var maxYear = 1899
+        for (year in years) {
+            sum += yearPopulations[year]!!
+            if (sum > maxSum) {
+                maxSum = sum
+                maxYear = year
+            }
+        }
+        return maxYear
     }
 
     private class Person(
@@ -12,10 +37,11 @@ class S10LivingPeople {
 
     fun runTest() {
         val people = arrayListOf(
-            Person(1905, 2000),
-            Person(1800, 1915),
-            Person(1817, 1917),
-            Person(1815, 1938),
+            Person(1900, 1907),
+            Person(1904, 1915),
+            Person(1930, 1987),
+            Person(1934, 1945),
+            Person(1942, 2000),
         )
         for (person in people) {
             println("[${person.birth}, ${person.death}]")

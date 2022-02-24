b3ea14d69fa706fbfcd43fdd7fc805c29c55a2cf
Raffi V <rafivar@gmail.com>
2022-02-24 14:16:04 +0200

    Added the book's solution

--- src/main/kotlin/chapter_8_recursion/S1TripleStep.kt
+++ src/main/kotlin/chapter_8_recursion/S1TripleStep.kt
@@ -1,6 +1,8 @@
 package chapter_8_recursion
 
 import Solution
+import java.util.*
+import kotlin.collections.ArrayList
 
 class S1TripleStep : Solution {
     private fun tripleSteps(numOfStairs: Int, possibleSteps: ArrayList<Int>): Int {
@@ -23,18 +25,37 @@ class S1TripleStep : Solution {
         return options
     }
 
+    private fun countWays(n: Int, possibleSteps: ArrayList<Int>): Int {
+        val memo = IntArray(n + 1)
+        Arrays.fill(memo, -1)
+        return countWays(n, possibleSteps, memo)
+    }
+
+    private fun countWays(n: Int, possibleSteps: ArrayList<Int>, memo: IntArray): Int {
+        return when {
+            n < 0 -> 0
+            n == 0 -> 1
+            memo[n] > -1 -> memo[n]
+            else -> {
+                memo[n] = countWays(n - possibleSteps[0], possibleSteps, memo) +
+                        countWays(n - possibleSteps[1], possibleSteps, memo) +
+                        countWays(n - possibleSteps[2], possibleSteps, memo)
+                memo[n]
+            }
+        }
+    }
+
     override fun runTest() {
-        val functions = arrayListOf(this::tripleSteps)
+        val functions = arrayListOf(this::tripleSteps, this::countWays)
         val testCases = arrayListOf(
             Pair(4, arrayListOf(1, 2, 3))
         )
         for (function in functions) {
             for (case in testCases) {
-                println("------------------------------------------")
+                println("------------------------------------------------------------------------------------")
                 println("stairs: ${case.first}")
                 println("step sizes: ${case.second}")
                 println("options to go up the stairs: ${function(case.first, case.second)}")
-                println("------------------------------------------")
             }
         }
     }
